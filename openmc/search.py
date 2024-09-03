39fa704e2b64d0b96c7a0d72ef5f1b862f929e11
gaspersrsen <gasper.srsen@hotmail.com>
2024-09-03 13:02:47 +0200

    Update search.py

--- openmc/search.py
+++ openmc/search.py
@@ -251,7 +251,7 @@ def custom_root_finder(f, x0, bracket, tol=1e-3, args=(), max_iter=50):
         else:
             give= left
             if np.abs(right.value)<np.abs(give.value):give=right
-            if np.abs(right.value)<np.abs(give.value):give=x0
+            if np.abs(next.value)<np.abs(give.value):give=next
             print(f"This range does not contain the root, returning closest value: f({give.guess})={give.value}")
             return give.guess
         
