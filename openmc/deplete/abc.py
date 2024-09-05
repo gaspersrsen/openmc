8a046d232b34e9c65e730d44d56c3859336fa88d
gaspersrsen <gasper.srsen@hotmail.com>
2024-09-05 13:16:54 +0200

    Update abc.py

--- openmc/deplete/abc.py
+++ openmc/deplete/abc.py
@@ -877,7 +877,7 @@ class Integrator(ABC):
                     self.operator.model = model_builder(self.operator.model, **model_args)
                     res2 = self.operator(n, source_rate)
                     print(f"[openmc.deplete] model k, updated after depletion step:{i} from k_deplete:{res_list[0].k} to k_model: {res2.k}")
-                    print(res_list[1],res2)
+                    print(res_list)
                     #res_list[1][0]=res2.k
 
                 StepResult.save(self.operator, n_list, res_list, [t, t + dt],
