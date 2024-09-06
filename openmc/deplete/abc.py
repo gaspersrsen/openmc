13ec05c945b23af05e52cd6813b7120974436393
gaspersrsen <gasper.srsen@hotmail.com>
2024-09-06 15:17:11 +0200

    Update abc.py

--- openmc/deplete/abc.py
+++ openmc/deplete/abc.py
@@ -855,31 +855,7 @@ class Integrator(ABC):
 
                 # Update the model, if not already corrected in the previous step
                 if ((model_builder is not None) and (i>0)) and (not correct_k_after_each_step):
-                    self.operator.model = model_builder(self.operator.model, **model_args)
-                else:
-                    # Solve transport equation (or obtain result from restart)
-                    openmc.lib.reset()
-                    if self._n_calls > 0:
-                        openmc.lib.reset_timers()
-            
-                    self._update_materials_and_nuclides(vec)
-                    self.operator.model = model_builder(self.operator.model, **model_args)
-            
-                    # If the source rate is zero, return zero reaction rates without running
-                    # a transport solve
-                    if source_rate == 0.0:
-                        rates = self.operator.reaction_rates.copy()
-                        rates.fill(0.0)
-                        res = OperatorResult(ufloat(0.0, 0.0), rates)
-            
-                    # Run OpenMC
-                    openmc.lib.run()
-                    # Extract results
-                    rates = self.operator._calculate_reaction_rates(source_rate)
-                    # Get k and uncertainty
-                    keff = ufloat(*openmc.lib.keff())
-                    res = OperatorResult(keff, rates)
-                    self.operator._n_calls += 1
+                    res = self.operator.run_wih_model(model_builder, model_args)
                 
                 # if i > 0 or self.operator.prev_res is None:
                 #     n, res = self._get_bos_data_from_operator(i, source_rate, n)
@@ -898,31 +874,11 @@ class Integrator(ABC):
                 res_list.insert(0, res)
                 # Update the model and correct the result k, i
                 if (model_builder is not None) and (correct_k_after_each_step):
-                    # Solve transport equation (or obtain result from restart)
-                    openmc.lib.reset()
-                    if self._n_calls > 0:
-                        openmc.lib.reset_timers()
-                    self._update_materials_and_nuclides(vec)
-                    self.operator.model = model_builder(self.operator.model, **model_args)
-                    # If the source rate is zero, return zero reaction rates without running
-                    # a transport solve
-                    if source_rate == 0.0:
-                        rates = self.operator.reaction_rates.copy()
-                        rates.fill(0.0)
-                        res = OperatorResult(ufloat(0.0, 0.0), rates)
-                    else:
-                    # Run OpenMC
-                        openmc.lib.run()
-                        # Extract results
-                        rates = self.operator._calculate_reaction_rates(source_rate)
-                        # Get k and uncertainty
-                        keff = ufloat(*openmc.lib.keff())
-                        res = OperatorResult(keff, rates)
-                        self.operator._n_calls += 1
-                        res = self.operator(n, source_rate)
-                        print(f"[openmc.deplete] k, updated at depletion step {i}: 
-                            from k(t={t}):{res_list[0].k} to k_model: {res2.k}")
-                        res_list[1]=res
+                    # Solve transport equation
+                    res2 = self.operator.run_with_model(model_builder, model_args)
+                    print(f"[openmc.deplete] k, updated at depletion step {i}: 
+                        from k(t={t}):{res_list[0].k} to k_model: {res2.k}")
+                    res_list[1]=res
                     #print(res_list)
                     #res_list[1][0]=res2.k
                 
