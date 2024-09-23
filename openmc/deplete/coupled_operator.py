74847e7c375d64c3c547abd45c41e9563627d248
gaspersrsen <gasper.srsen@hotmail.com>
2024-09-23 11:15:14 +0200

    Update coupled_operator.py

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -497,6 +497,7 @@ class CoupledOperator(OpenMCOperator):
                 tallies = openmc.lib.tallies
                 for i in tallies.values():
                     print(f"{np.array2string(i.mean, formatter={'float': lambda x: f'{x:.5f}'}, separator=', ')}")
+                    print(f"{np.array2string(i.results[-1], formatter={'float': lambda x: f'{x:.5f}'}, separator=', ')}")
                 # Determine change of concentration
                 if invert_k*(k[0]-target) < 0: 
                     if direction != 0:
