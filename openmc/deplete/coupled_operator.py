17ef1998b808426c93252a8d48cee815d6106bff
gaspersrsen <gasper.srsen@hotmail.com>
2024-09-27 16:04:52 +0200

    test

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -514,7 +514,7 @@ class CoupledOperator(OpenMCOperator):
                     if i == 2:
                         break
                     #print(tally_.results - prev_res[i],tally_.results,prev_res[i])
-                    curr_res += [tally_.results]# - prev_res[i]]
+                    curr_res += [tally_.results - prev_res[i]]
                     prev_res[i] = curr_res[i]
                     i += 1
                 print(curr_res)
@@ -524,15 +524,16 @@ class CoupledOperator(OpenMCOperator):
                 glob_tall = openmc.lib.global_tallies()
                 print(glob_tall)
                 P_fiss = curr_res[0][0][0][1]
-                P_nxn = curr_res[0][0][2][1] + 2*curr_res[0][0][3][1]
+                P_nxn = curr_res[0][0][2][1] + 2.0*curr_res[0][0][3][1]
                 L_leak = glob_tall[3][0]
                 L_abs = curr_res[0][0][1][1]
                 L_abs_nucs = np.sum(np.array(curr_res[1][0]), axis=1)[1]
                 print(P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs)
                 
-                #g = ((P_fiss/target +  P_nxn) * (1-L_leak) - (L_abs-L_abs_nucs)) / (L_abs_nucs)
+                g = ((P_fiss/target +  P_nxn) * (1-L_leak) - (L_abs-L_abs_nucs)) / (L_abs_nucs)
                 #f *= g
                 print(g, f)
+                g = 1
                 # Determine change of concentration
                 # if invert_k*(k[0]-target) < 0: 
                 #     if direction != 0:
