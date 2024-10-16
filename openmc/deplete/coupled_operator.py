8cf21e4f2a240c9a0689ca7b4e9a2641ade68472
gaspersrsen <gasper.srsen@hotmail.com>
2024-10-16 14:23:47 +0200

    test

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -526,16 +526,16 @@ class CoupledOperator(OpenMCOperator):
                 prev_leak = glob_tall[3][0]*M
                 
                 P_fiss = (curr_res[0][0][0][1])/k[0]
-                P_nxn = (curr_res[0][0][2][1] - curr_res[0][0][3][1])/k[0]
+                P_nxn = (curr_res[0][0][2][1] - curr_res[0][0][3][1])#/k[0]
                 L_leak = leak # Fraction
-                L_abs = curr_res[0][0][1][1]/k[0]
-                L_abs_nucs = np.sum(np.sum(np.array(curr_res[1][0]).T, axis=1))/k[0]
+                L_abs = curr_res[0][0][1][1]#/k[0]
+                L_abs_nucs = np.sum(np.sum(np.array(curr_res[1][0]).T, axis=1))#/k[0]
                 print(P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs)
                 
-                if M > 5:
-                    res_avg += [[P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs]]
-                    [P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs] = np.average(np.array(res_avg).T, axis=1)
-                print(P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs)
+                # if M > 5:
+                #     res_avg += [[P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs]]
+                #     [P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs] = np.average(np.array(res_avg).T, axis=1)
+                # print(P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs)
                 # #OPTIMAL FOLLOWING
                 # if M == 1:
                 #     #opt_vari = np.abs((target-k[0])/target)**2
