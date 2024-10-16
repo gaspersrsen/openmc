3010fa3c43d89a97c2ae9a417bb74a601cbba691
gaspersrsen <gasper.srsen@hotmail.com>
2024-10-16 17:31:25 +0200

    test

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -530,10 +530,10 @@ class CoupledOperator(OpenMCOperator):
                 prev_leak = glob_tall[3][0]*M
                 
                 P_fiss = (curr_res[0][0][0][1])/k
-                P_nxn = (curr_res[0][0][2][1] - curr_res[0][0][3][1])/k
+                P_nxn = (curr_res[0][0][2][1] - curr_res[0][0][3][1])#/k
                 L_leak = leak # Fraction
-                L_abs = curr_res[0][0][1][1]/k
-                L_abs_nucs = np.sum(np.sum(np.array(curr_res[1][0]).T, axis=1))/k
+                L_abs = curr_res[0][0][1][1]#/k
+                L_abs_nucs = np.sum(np.sum(np.array(curr_res[1][0]).T, axis=1))#/k
                 print(P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs)
                 
                 if M > 5:
