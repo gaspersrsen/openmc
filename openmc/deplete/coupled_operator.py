4464a97f2f623efa15ec7f6abafc3cfdd60d983d
gaspersrsen <gasper.srsen@hotmail.com>
2024-10-25 16:37:37 +0200

    test

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -535,8 +535,8 @@ class CoupledOperator(OpenMCOperator):
                 L_abs_nucs = np.sum(np.sum(np.array(curr_res[1][0]).T, axis=1))
                 print(P_fiss_prompt, P_fiss_delayed, P_nxn, L_leak, L_abs, L_abs_nucs)
                 #Calculate the conc change for this batch only
-                k = (P_fiss_prompt/target + P_fiss_delayed)/((P_fiss_prompt/target + P_fiss_delayed)*(leak)+L_abs)
-                dk = ((curr_res[0][0][0][2] + curr_res[0][0][1][2])/(curr_res[0][0][0][1] + curr_res[0][0][1][1]) + curr_res[0][0][2][2]/curr_res[0][0][2][1])
+                k = (P_fiss_prompt + P_fiss_delayed)/((P_fiss_prompt + P_fiss_delayed)*(leak)+L_abs)
+                dk = (((curr_res[0][0][0][2]) + curr_res[0][0][1][2])/(curr_res[0][0][0][1] + curr_res[0][0][1][1]) + curr_res[0][0][2][2]/curr_res[0][0][2][1])
                    
                 print(f"k={k} +/- {dk*k}")
                 corr = ((P_fiss_prompt/target + P_fiss_delayed + 0*P_nxn) * (1-L_leak) - (L_abs-L_abs_nucs)) / L_abs_nucs
@@ -544,7 +544,7 @@ class CoupledOperator(OpenMCOperator):
                 if g <= 0:
                     g = 0.1
                 #Optimal following:
-                p_measure = (np.absolute(k/target - 1) + dk)**2
+                p_measure = 1/self.model.settings.particles
                 z = f_prev * g 
                 if M == 1:
                     x = 0
