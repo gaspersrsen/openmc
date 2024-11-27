b1e92eb6fea940e5bfb3dd8a625a6e8f5f502370
gaspersrsen <gasper.srsen@hotmail.com>
2024-11-27 15:20:29 +0100

    test

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -530,8 +530,8 @@ class CoupledOperator(OpenMCOperator):
                 L_abs = curr_res[0][0][2][1]
                 L_abs_nucs = np.sum(np.sum(np.array(curr_res[1][0]).T, axis=1))
                 #Calculate the conc change for this batch only
-                corr = (((P_fiss_prompt + P_fiss_delayed)/target + 0*P_nxn) * (1-L_leak) - (L_abs-L_abs_nucs))/ L_abs_nucs
                 k = (P_fiss_prompt + P_fiss_delayed)/((L_abs)/(1-leak) - 0*P_nxn)
+                corr = (((P_fiss_prompt + P_fiss_delayed)/target + 0*P_nxn) * (1-L_leak) - (L_abs-L_abs_nucs))/ L_abs_nucs * k/target
                 g = corr
                 if g <= 0:
                     g = 0.5
