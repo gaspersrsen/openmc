dd5cc05900af79048c13726a1e8ebf4deded75d9
gaspersrsen <gasper.srsen@hotmail.com>
2024-12-05 16:25:41 +0100

    test

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -514,7 +514,7 @@ class CoupledOperator(OpenMCOperator):
                 L_abs_nucs = np.sum(np.array(curr_res[1][0]).T, axis=1)[1]
                 #Calculate the conc change for this batch only
                 nxn = 1
-                k = (P_fiss) / (L_abs + (P_fiss + 0*nxn*P_nxn)*L_leak - 0*nxn*P_nxn)
+                k = (P_fiss) / (L_abs + (P_fiss + 1*nxn*P_nxn)*L_leak - 1*nxn*P_nxn)
                 g = ((P_fiss/target + nxn*P_nxn)
                         - (L_abs - L_abs_nucs) - (P_fiss + nxn*P_nxn)*L_leak) / L_abs_nucs * k/target
                 print(g)
