99465002c46c7227167734e8ec76beba557710f9
gaspersrsen <gasper.srsen@hotmail.com>
2024-12-16 17:40:37 +0100

    test

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -518,10 +518,15 @@ class CoupledOperator(OpenMCOperator):
                 g = ((P_fiss/target + P_nxn)
                         - (L_abs - L_abs_nucs) - (P_fiss + P_nxn)*L_leak) / L_abs_nucs #* k/target
                 print(g)
-                nucs_err= np.sum(np.array(curr_res[1][0]).T, axis=1)[2]
-                print(g**2*(((curr_res[0][0][0][2]/target)**2 + curr_res[0][0][2][2]**2 + curr_res[0][0][3][2]**2 - 2*curr_res[0][0][2][2]*curr_res[0][0][3][2]) * (1-L_leak)))# +
-                print((curr_res[0][0][1][2]**2 + nucs_err**2 - 2*curr_res[0][0][1][2]*nucs_err)/(g*L_abs_nucs)**2 + nucs_err**2/L_abs_nucs**2)# -
-                print(2*(nucs_err*np.sqrt(curr_res[0][0][1][2]**2 + nucs_err**2 - 2*nucs_err*curr_res[0][0][1][2]))/(g*L_abs_nucs**2))
+                nucs_err1 = np.sum(np.array(curr_res[1][0]).T, axis=1)[2]
+                fiss_err = (curr_res[0][0][0][2]/target)**2
+                nxn_err = curr_res[0][0][2][2]**2 + curr_res[0][0][3][2]**2 - 2*curr_res[0][0][2][2]*curr_res[0][0][3][2]
+                abs_err = curr_res[0][0][1][2]**2 + nucs_err1**2 - 2*curr_res[0][0][1][2]*nucs_err1
+                top_err = fiss_err + nxn_err + abs_err - 2*(np.sqrt(fiss_err*nxn_err) + np.sqrt(fiss_err*abs_err) + np.sqrt(nxn_err*abs_err))
+                print(g**2*(top_err/(g*L_abs_nucs)**2 + (nucs_err1/L_abs_nucs)**2 - 2*np.sqrt(top_err)*nucs_err1/(g*L_abs_nucs**2)))
+                #print(g**2*(((curr_res[0][0][0][2]/target)**2 + curr_res[0][0][2][2]**2 + curr_res[0][0][3][2]**2 - 2*curr_res[0][0][2][2]*curr_res[0][0][3][2]) * (1-L_leak)))# +
+                #print((curr_res[0][0][1][2]**2 + nucs_err**2 - 2*curr_res[0][0][1][2]*nucs_err)/(g*L_abs_nucs)**2 + nucs_err**2/L_abs_nucs**2)# -
+                #print(2*(nucs_err*np.sqrt(curr_res[0][0][1][2]**2 + nucs_err**2 - 2*nucs_err*curr_res[0][0][1][2]))/(g*L_abs_nucs**2))
                 #Optimal following:
                 if (k-target < 0) != (g < 1):
                     print("WIERD")
