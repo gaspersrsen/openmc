fa236a7745eadc3ac31a049cec4cac0578b4a88e
gaspersrsen <gasper.srsen@hotmail.com>
2024-12-20 15:35:42 +0100

    a

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -518,7 +518,7 @@ class CoupledOperator(OpenMCOperator):
                 # Predict concentration change
                 k = (P_fiss + P_nxn) / (L_abs + (P_fiss + P_nxn)*L_leak)
                 g = ((P_fiss/target + P_nxn)
-                        - (L_abs - L_abs_nucs) - (P_fiss + P_nxn)*L_leak) / L_abs_nucs * np.exp(k/target)
+                        - (L_abs - L_abs_nucs) - (P_fiss + P_nxn)*L_leak) / L_abs_nucs * np.exp(k-target)
                 print(g)
                 # Optimal following (Kalman filter for narrowing to a scalar value):
                 if M == 10:
