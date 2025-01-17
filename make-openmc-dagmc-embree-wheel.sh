d6304ca89e711976b7845d6c485898d7ad092d67
gaspersrsen <gasper.srsen@hotmail.com>
2025-01-17 19:00:16 +0100

    a

--- make-openmc-dagmc-embree-wheel.sh
+++ make-openmc-dagmc-embree-wheel.sh
@@ -219,10 +219,10 @@ else
         -DBUILD_TESTING=OFF \
         -DOPENMC_USE_LIBMESH=$build_libmesh \
         -DCMAKE_PREFIX_PATH="${DAGMC_INSTALL_DIR};${LIBMESH_INSTALL_DIR}" \
-        #-DCMAKE_PREFIX_PATH="/usr/local;${LIBMESH_INSTALL_DIR}" \
         -DXTENSOR_USE_TBB=OFF \
         -DXTENSOR_USE_OPENMP=ON \
         -DXTENSOR_USE_XSIMD=OFF
+    # -DCMAKE_PREFIX_PATH="/usr/local;${LIBMESH_INSTALL_DIR}" \
     # Continue installation even if the build failed. At the moment, the build fails on 90% because
     # it can not find catch2 lib when building the tests.
     cmake --build . --parallel "$(nproc)"
