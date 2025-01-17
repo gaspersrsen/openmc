6e8507d7e748680b619e9638b6eaeccf8c0bb58a
gaspersrsen <gasper.srsen@hotmail.com>
2025-01-17 12:59:53 +0100

    a

--- make-openmc-dagmc-embree-wheel.sh
+++ make-openmc-dagmc-embree-wheel.sh
@@ -170,30 +170,30 @@ else
     cmake --install .
 fi
 
-# # Install DAGMC
-# cd $SRC_DIR
-# if build_exists "$SRC_DIR/DAGMC/build"; then
-#     echo "DAGMC already built."
-# else
-#     git clone --single-branch --branch v3.2.4 --depth 1 https://github.com/svalinn/DAGMC.git || echo "DAGMC already cloned."
-#     mkdir -p DAGMC/build
-#     cd DAGMC/build
-#     cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/ \
-#              -DCMAKE_BUILD_TYPE=Release \
-#              -DBUILD_TALLY=ON \
-#              -DBUILD_TESTS=OFF \
-#              -DBUILD_EXE=OFF \
-#              -DBUILD_BUILD_OBB=OFF \
-#              -DMOAB_DIR=/usr/local/ \
-#              -DDOUBLE_DOWN=ON \
-#              -DDOUBLE_DOWN_DIR=/usr/local/ \
-#              -DOpenMP_pthread_LIBRARY=/lib/x86_64-linux-gnu/libpthread.so.0 \
-#              -DBUILD_STATIC_EXE=OFF \
-#              -DBUILD_STATIC_LIBS=OFF
-#     cmake --build . --parallel "$(nproc)"
-#     cmake --install .
-# fi
-# echo "Script completed"
+# Install DAGMC
+cd $SRC_DIR
+if build_exists "$SRC_DIR/DAGMC/build"; then
+    echo "DAGMC already built."
+else
+    git clone --single-branch --branch v3.2.4 --depth 1 https://github.com/svalinn/DAGMC.git || echo "DAGMC already cloned."
+    mkdir -p DAGMC/build
+    cd DAGMC/build
+    cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/ \
+             -DCMAKE_BUILD_TYPE=Release \
+             -DBUILD_TALLY=ON \
+             -DBUILD_TESTS=OFF \
+             -DBUILD_EXE=OFF \
+             -DBUILD_BUILD_OBB=OFF \
+             -DMOAB_DIR=/usr/local/ \
+             -DDOUBLE_DOWN=ON \
+             -DDOUBLE_DOWN_DIR=/usr/local/ \
+             -DOpenMP_pthread_LIBRARY=/lib/x86_64-linux-gnu/libpthread.so.0 \
+             -DBUILD_STATIC_EXE=OFF \
+             -DBUILD_STATIC_LIBS=OFF
+    cmake --build . --parallel "$(nproc)"
+    cmake --install .
+fi
+echo "Script completed"
 
 # # Install OpenMC (C++ core)
 # cd $SRC_DIR
