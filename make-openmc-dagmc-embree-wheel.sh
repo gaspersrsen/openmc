190904bab7039decb07cebc17436db9b1b8f05c6
gaspersrsen <gasper.srsen@hotmail.com>
2025-01-30 18:16:32 +0100

    a

--- make-openmc-dagmc-embree-wheel.sh
+++ make-openmc-dagmc-embree-wheel.sh
@@ -79,6 +79,7 @@ wget http://deb.debian.org/debian/pool/non-free/p/parmetis/parmetis_4.0.3-7.debi
 #tar -xvf parmetis_4.0.3-7.debian.tar.xz
 wget http://ftp.debian.org/debian/pool/non-free/p/parmetis/parmetis_4.0.3-7.dsc
 dpkg-source -x parmetis_4.0.3-7.dsc
+cd ..
 dpkg-buildpackage -rfakeroot -b
 dpkg -i ../parmetis_4.0.3-7_amd64.deb
 # make config shared=1 -j"$(nproc)"
