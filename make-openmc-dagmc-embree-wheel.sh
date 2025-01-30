9c7926a8a70f08595c31fdbf16ecc5713c3d6585
gaspersrsen <gasper.srsen@hotmail.com>
2025-01-30 16:06:28 +0100

    a

--- make-openmc-dagmc-embree-wheel.sh
+++ make-openmc-dagmc-embree-wheel.sh
@@ -47,9 +47,7 @@ tar -xvzf parmetis_4.0.3.orig.tar.gz
 cd parmetis-4.0.3/
 make config shared=1 -j"$(nproc)"
 make install
-#cd $SRC_DIR
-mkdir metis
-cd parmetis_4.0.3/metis
+cd metis
 make config prefix=$SRC_DIR/metis
 make install
 # pip3 install metis
