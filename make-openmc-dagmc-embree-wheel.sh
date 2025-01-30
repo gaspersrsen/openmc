ebd90d55c1196e6dcd3fc0e4c1482777ed2bad97
gaspersrsen <gasper.srsen@hotmail.com>
2025-01-30 17:33:19 +0100

    a

--- make-openmc-dagmc-embree-wheel.sh
+++ make-openmc-dagmc-embree-wheel.sh
@@ -52,6 +52,7 @@ apt-get install -y cmake \
                         libhdf5-serial-dev \
                         libmpich-dev \
                         libmetis-dev \
+                        libparmetis4.0 \
                         libnetcdf-dev \
                         libnetcdf-mpi-dev \
                         libopenblas-dev \
