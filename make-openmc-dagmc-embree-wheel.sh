acdc40ec287349227dd00c112f5872ff3e82e977
gaspersrsen <gasper.srsen@hotmail.com>
2025-02-13 19:38:15 +0100

    a

--- make-openmc-dagmc-embree-wheel.sh
+++ make-openmc-dagmc-embree-wheel.sh
@@ -54,7 +54,7 @@ fi
 #                         debhelper-compat=13 \
 #                         libc6-dev \
 #                         apt-utils
-apt-get -y install libeigen3-dev libnetcdf-dev libtbb-dev libglfw3-dev libmetis-dev libparmetis-dev
+apt-get -y install libeigen3-dev libnetcdf-dev libtbb-dev libglfw3-dev libmetis-dev libscotchparmetis-dev
 
 
 # #apt-get update -y
