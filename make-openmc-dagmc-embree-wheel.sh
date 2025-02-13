c563d25e501c0c662a040f326f1442551eb3ef29
gaspersrsen <gasper.srsen@hotmail.com>
2025-02-13 15:59:15 +0100

    a

--- make-openmc-dagmc-embree-wheel.sh
+++ make-openmc-dagmc-embree-wheel.sh
@@ -139,35 +139,35 @@ else
 fi
 
 # Install MOAB (with PyMOAB enabled)
-cd $SRC_DIR
-if build_exists "$SRC_DIR/moab/build"; then
-    echo "MOAB already built."
-else
-    git clone --single-branch -b 5.5.1 --depth 1 https://bitbucket.org/fathomteam/moab/ || echo "MOAB already cloned."
-    mkdir -p moab/build
-    cd moab/build
-    cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/ \
-             -DCMAKE_BUILD_TYPE=Release \
-             -DENABLE_HDF5=ON \
-             -DENABLE_PYMOAB=ON \
-             -DENABLE_BLASLAPACK=OFF \
-             -DENABLE_FORTRAN=OFF \
-             -DENABLE_METIS=ON \
-             -DENABLE_MPI=ON \
-             -DENABLE_NETCDF=ON \
-             -DENABLE_PARMETIS=ON \
-             -DENABLE_PNETCDF=OFF
-    cmake --build . --parallel "$(nproc)"
-    cmake --install .
-fi
+# cd $SRC_DIR
+# if build_exists "$SRC_DIR/moab/build"; then
+#     echo "MOAB already built."
+# else
+#     git clone --single-branch -b 5.5.1 --depth 1 https://bitbucket.org/fathomteam/moab/ || echo "MOAB already cloned."
+#     mkdir -p moab/build
+#     cd moab/build
+#     cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/ \
+#              -DCMAKE_BUILD_TYPE=Release \
+#              -DENABLE_HDF5=ON \
+#              -DENABLE_PYMOAB=ON \
+#              -DENABLE_BLASLAPACK=OFF \
+#              -DENABLE_FORTRAN=OFF \
+#              -DENABLE_METIS=ON \
+#              -DENABLE_MPI=ON \
+#              -DENABLE_NETCDF=ON \
+#              -DENABLE_PARMETIS=ON \
+#              -DENABLE_PNETCDF=OFF
+#     cmake --build . --parallel "$(nproc)"
+#     cmake --install .
+# fi
 
-if [ "$BUILD_WHEELS" = true ]; then
-    # Build and install PyMOAB wheel
+# if [ "$BUILD_WHEELS" = true ]; then
+#     # Build and install PyMOAB wheel
 
-    chmod -R 777 $SRC_DIR/moab/build
-    cd $SRC_DIR/moab/build/pymoab
-    python -m build --wheel --outdir "$WHEEL_DIR"
-fi
+#     chmod -R 777 $SRC_DIR/moab/build
+#     cd $SRC_DIR/moab/build/pymoab
+#     python -m build --wheel --outdir "$WHEEL_DIR"
+# fi
 
 # Install Double-Down
 # cd $SRC_DIR
