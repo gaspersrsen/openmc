83f2322fdc2e5bb4a19ff5197824c54808438aa2
gaspersrsen <gasper.srsen@hotmail.com>
2025-02-13 15:10:43 +0100

    a

--- make-openmc-dagmc-embree-wheel.sh
+++ make-openmc-dagmc-embree-wheel.sh
@@ -185,28 +185,28 @@ else
     cmake --install .
 fi
 
-# Install DAGMC
-cd $SRC_DIR
-if build_exists "$SRC_DIR/DAGMC/build"; then
-    echo "DAGMC already built."
-else
-    git clone --single-branch --branch v3.2.4 --depth 1 https://github.com/svalinn/DAGMC.git || echo "DAGMC already cloned."
-    mkdir -p DAGMC/build
-    cd DAGMC/build
-    cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/ \
-             -DCMAKE_BUILD_TYPE=Release \
-             -DBUILD_TALLY=ON \
-             -DBUILD_TESTS=OFF \
-             -DBUILD_EXE=OFF \
-             -DBUILD_BUILD_OBB=OFF \
-             -DMOAB_DIR=/usr/local/ \
-             -DDOUBLE_DOWN=ON \
-             -DDOUBLE_DOWN_DIR=/usr/local/ \
-             -DBUILD_STATIC_EXE=OFF \
-             -DBUILD_STATIC_LIBS=OFF
-    cmake --build . --parallel "$(nproc)"
-    cmake --install .
-fi
+# # Install DAGMC
+# cd $SRC_DIR
+# if build_exists "$SRC_DIR/DAGMC/build"; then
+#     echo "DAGMC already built."
+# else
+#     git clone --single-branch --branch v3.2.4 --depth 1 https://github.com/svalinn/DAGMC.git || echo "DAGMC already cloned."
+#     mkdir -p DAGMC/build
+#     cd DAGMC/build
+#     cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/ \
+#              -DCMAKE_BUILD_TYPE=Release \
+#              -DBUILD_TALLY=ON \
+#              -DBUILD_TESTS=OFF \
+#              -DBUILD_EXE=OFF \
+#              -DBUILD_BUILD_OBB=OFF \
+#              -DMOAB_DIR=/usr/local/ \
+#              -DDOUBLE_DOWN=ON \
+#              -DDOUBLE_DOWN_DIR=/usr/local/ \
+#              -DBUILD_STATIC_EXE=OFF \
+#              -DBUILD_STATIC_LIBS=OFF
+#     cmake --build . --parallel "$(nproc)"
+#     cmake --install .
+# fi
 
 # Install LIBMESH
 # apt-get -y install m4 libnetcdf-dev libpnetcdf-dev 
