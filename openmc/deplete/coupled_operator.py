816780522bb8b662d8179a080b160ce1b02cebe5
gaspersrsen <gasper.srsen@hotmail.com>
2025-02-13 13:30:15 +0100

    a

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -519,7 +519,7 @@ class CoupledOperator(OpenMCOperator):
                 #k = (P_fiss) / (L_abs + P_fiss*L_leak)
                 k = (P_fiss + P_nxn) / (L_abs + (P_fiss + P_nxn)*L_leak)
                 g = ((P_fiss/target + P_nxn)
-                        - (L_abs - L_abs_nucs) - (P_fiss + P_nxn)*L_leak) / L_abs_nucs #* np.exp(k-target)
+                        - (L_abs - L_abs_nucs) - (P_fiss + P_nxn)*L_leak) / L_abs_nucs * np.exp(k-target)
                 print(g, k)
                 # Optimal following (Kalman filter for narrowing to a scalar value):
                 if M == 10:
