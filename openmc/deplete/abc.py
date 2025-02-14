c29a09b18e9aabfd9a11ebd7d3468363a6e64107
gaspersrsen <gasper.srsen@hotmail.com>
2025-02-14 14:45:37 +0100

    a

--- openmc/deplete/abc.py
+++ openmc/deplete/abc.py
@@ -859,7 +859,6 @@ class Integrator(ABC):
                 tallyTest2.nuclides = conc_args["iso"]
                 tallyTest2.scores = ["absorption"]
                 if "materials" in conc_args.keys():
-                    print("HI")
                     tallyTest2.filters = [MaterialFilter(conc_args["materials"],filter_id=8888)]
                 self.operator.model.tallies += [tallyTest2]
                 self.operator.model.tallies.export_to_xml()
