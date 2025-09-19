90a08142a790160224ba6ecb23c1e3a1900d8945
gaspersrsen <gasper.srsen@hotmail.com>
2025-09-19 15:42:22 +0200

    a

--- openmc/deplete/coupled_operator.py
+++ openmc/deplete/coupled_operator.py
@@ -550,16 +550,16 @@ class CoupledOperator(OpenMCOperator):
                     # Sig = part_tally * rel_err
                     # rel_err = sqrt(1/N_part_tally) = 1/sqrt(N_tot) * sqrt(N_tot/N_part_tally) = rel_err_MC * sqrt(N_tot/N_part_tally) = rel_err_MC * sqrt(tot_tally/part_tally)
                     # Sig = part_tally * rel_err_MC * sqrt(tot/part_tally) = rel_err_MC * sqrt(tot*part_tally)
-                    sig1 = rel_err_MC * (np.sqrt(prod / P_fiss)/target + np.sqrt(prod / P_nxn)) #sig for (P_fiss + P_nxn)/target
-                    sig2 = rel_err_MC * (np.sqrt(loss / L_abs) + np.sqrt(loss / L_abs_nucs) ) #sig for (L_abs - L_abs_nucs)
+                    sig1 = rel_err_MC * (np.sqrt(prod * P_fiss)/target + np.sqrt(prod * P_nxn)) #sig for (P_fiss + P_nxn)/target
+                    sig2 = rel_err_MC * (np.sqrt(loss * L_abs) + np.sqrt(loss * L_abs_nucs) ) #sig for (L_abs - L_abs_nucs)
                     #logic: prod = loss = abs + leak; leak = prod - abs
                     #sig_leak = rel_err_MC*(np.sqrt(prod/P_fiss) + np.sqrt(prod/P_nxn)) + rel_err_MC*np.sqrt(loss/L_abs) #sig for L_leak
                     #sig3 = (sig1/prod + sig_leak/loss) * prod * L_leak  #sig for (P_fiss + P_nxn)/target * L_leak; Sig = L_leak * prod * (rel_err(prod) + rel_err(L_leak)) / target
-                    sig3 = rel_err_MC * (np.sqrt(prod / P_fiss) + np.sqrt(prod / P_nxn)) * L_leak
+                    sig3 = rel_err_MC * (np.sqrt(prod * P_fiss) + np.sqrt(prod * P_nxn)) * L_leak
                     #Division by target must not influence relative errors
                     rel_err_top = (sig1 + sig2 + sig3) / top
-                    rel_err_bot = rel_err_MC * np.sqrt(loss / L_abs_nucs) / bot
-                    rel_err_g_est = rel_err_top + rel_err_bot
+                    rel_err_bot = rel_err_MC * np.sqrt(loss * L_abs_nucs) / bot
+                    rel_err_g_est = (rel_err_top + rel_err_bot)  * (1+100*np.exp(-(M-10)**2/(batches/6))) #Slowly relax uncertainty, as first guesses are inaccurate, about 2/3 of batches get very little extra uncertainty, this improves convergence when initial guess is bad, but increases final uncertainty
                     sig_g_est = f_prev * g_est * rel_err_g_est
                     p_measure = sig_g_est**2
                     
@@ -592,8 +592,11 @@ class CoupledOperator(OpenMCOperator):
                     k = (P_fiss + P_nxn) / (L_abs + (P_fiss + P_nxn)*L_leak)
                     print(f"Batch: {M}")
                     print(f"k_absorption:{k}")
+                    print(f"Batch estimated correction:{g_est}")
+                    print(f"Batch filtered correction:{f_prev/f}")
                     print(f"Search algorithm internal tally:\n{curr_res}")
                     print(f"Correction coefficients [P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs]: {P_fiss, P_nxn, L_leak, L_abs, L_abs_nucs}")
+                    print(f"Sigmas: [sig1, sig2, sig3]: {sig1, sig2, sig3}")
                     print(f"Batch concentration correction:{g}")
                     print(f"Batch estimated concentration:{f*initial_value} +/- {f*initial_value*(p**(1/2))}")
 
