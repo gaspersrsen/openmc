handled possible neagative leakage, some other fixes

openmc/deplete/coupled_operator.py
@@ -522,7 +522,7 @@ class CoupledOperator(OpenMCOperator):
                 
                 P_fiss = curr_res[0][0][0][1]                               # Neutrons produced by fission (prompt and delayed)
                 P_nxn = curr_res[0][0][2][1] - curr_res[0][0][3][1]         # Additional neutrons produced by (n,xn) reactions
-                L_leak = leak                                               # Neutron leakage fraction
+                L_leak = np.abs(leak)                                       # Neutron leakage fraction
                 L_abs = curr_res[0][0][1][1]                                # Total neutron absorption
                 # Total flagged nuclide absorption
                 if materials is not None:
@@ -556,9 +556,9 @@ class CoupledOperator(OpenMCOperator):
                     #sig3 = (sig1/prod + sig_leak/loss) * prod * L_leak  #sig for (P_fiss + P_nxn)/target * L_leak; Sig = L_leak * prod * (rel_err(prod) + rel_err(L_leak)) / target
                     sig3 = rel_err_MC * (np.sqrt(prod * P_fiss) + np.sqrt(prod * P_nxn)) * L_leak
                     #Division by target must not influence relative errors
-                    rel_err_top = (sig1 + sig2 + sig3) / top
+                    rel_err_top = (np.abs(sig1) + np.abs(sig2) + np.abs(sig3)) / top
                     rel_err_bot = rel_err_MC * np.sqrt(loss * L_abs_nucs) / bot
-                    rel_err_g_est = (rel_err_top + rel_err_bot)  * (1+100*np.exp(-(M - 10)**2 / (batches / 6)) if (M - 10) < batches / 3 else 1) #Slowly relax uncertainty, as first guesses are inaccurate, 2/3 of batches do not extra uncertainty, this improves convergence when initial guess is bad, but increases final uncertainty
+                    rel_err_g_est = (rel_err_top + rel_err_bot)  * (100*np.exp(-(M - 10)**2 / (batches / 6)) if (M - 10) < batches / 3 else 1) #Slowly relax uncertainty, as first guesses are inaccurate, 2/3 of batches do not extra uncertainty, this improves convergence when initial guess is bad, but increases final uncertainty
                     sig_g_est = f_prev * g_est * rel_err_g_est
                     p_measure = sig_g_est**2
                     