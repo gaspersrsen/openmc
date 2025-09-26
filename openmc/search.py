8a7a1c6a0b5b8acb12dfff4d1731b46feb6d5da3
gaspersrsen <gasper.srsen@hotmail.com>
2025-09-26 10:32:43 +0200

    a

--- openmc/search.py
+++ openmc/search.py
@@ -513,7 +513,7 @@ def get_ao_mix_materials(materials, fracs: Iterable[float], fracs_target: Iterab
         cv.check_type('materials', materials, Iterable, Material)
         cv.check_type('fracs', fracs, Iterable, Real)
         # cv.check_type('fracs', fracs_target, Iterable, str)
-        cv.check_value('percent type', percent_type, {'ao', 'wo', 'vo'})
+        cv.check_value('percent type', percent_type, Iterable, {'ao', 'wo', 'vo'})
 
         fracs = np.asarray(fracs)
         
