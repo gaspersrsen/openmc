74a470bb8a405c6a124d9c158da915c92fe1bd32
gaspersrsen <gasper.srsen@hotmail.com>
2025-09-30 14:12:24 +0200

    a

--- openmc/search.py
+++ openmc/search.py
@@ -659,6 +659,7 @@ def get_wo_fraction(material):
 def update_material(mat, nuc_dict):
     for nuc in mat.nuclides:
         mat.remove_nuclide(nuc.name)
+    return 0
     for nuc, val in nuc_dict.items():
         mat.add_nuclide(nuc, val)
     nuc_dict2 = mat.get_nuclide_atom_densities()
