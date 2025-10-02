7384e6ae4b0edc71c16a4f4ad879385826e97361
gaspersrsen <gasper.srsen@hotmail.com>
2025-10-02 12:29:02 +0200

    a

--- openmc/search.py
+++ openmc/search.py
@@ -503,6 +503,7 @@ def critical_density_iteration(model, iso=None, batches=None, bracket=None,
                 nuclides=[]
                 densities=[]
                 all_nuc = np.array(openmc.lib.materials[int(mat)].nuclides)
+                mat_internal = openmc.lib.materials[int(mat)]
                 
                 if mat_builder is None:
                     all_dens = (np.array(openmc.lib.materials[int(mat)].densities)).astype(float)
@@ -521,9 +522,8 @@ def critical_density_iteration(model, iso=None, batches=None, bracket=None,
                 else:
                     for matpy in materials:
                         matpy_nuc_dict = matpy.get_nuclide_atom_densities()
-                        print("Mat change ids:",matpy.id,int(mat),matpy.id == int(mat))
                         if matpy.id == int(mat):
-                            print(*zip(mat.nuclides, mat.densities))
+                            print(*zip(mat_internal.nuclides, mat_internal.densities))
                             for nuc in all_nuc:
                                 val = matpy_nuc_dict.get(str(nuc),0)
                                 # If nuclide is zero, do not add to the problem.
@@ -531,11 +531,9 @@ def critical_density_iteration(model, iso=None, batches=None, bracket=None,
                                     nuclides.append(nuc)
                                     densities.append(val)
                             break
-                mat_internal = openmc.lib.materials[int(mat)]
                 mat_internal.set_density(np.sum(densities))
                 mat_internal.set_densities(nuclides, densities)
                 print(*zip(mat_internal.nuclides, mat_internal.densities))
-                print(*zip(mat.nuclides, mat.densities))
         # if M == model.settings.inactive:
         #     openmc.lib.reset()
     openmc.lib.simulation_finalize()
